8807d9ad1e0f809b708aa3d706a92001c004229b
Alexey <geheim79@mail.ru>
2026-07-18 10:51:32 +0500

    Подключен React Router и настроены первые маршруты

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+// импорт библиотек маршрутизации
+import { Routes, Route } from "react-router-dom";
+
+// импорт компонент для страниц
+import Home from "./pages/Home/Home";
+import Favorites from "./pages/Favorites/Favorites";
+import MovieDetails from "./pages/MovieDetails/MovieDetails";
+
 function App() {
   return (
     <div>
       <h1>Movie Search App</h1>
+      {/* создание таблицы маршрутов */}
+      <Routes>
+        {/* прописываем пути для страниц */}
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,5 @@
+function MovieDetails() {
+  return <h1>Страница фильма</h1>;
+}
+
+export default MovieDetails;

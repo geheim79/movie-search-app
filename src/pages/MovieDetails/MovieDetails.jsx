38e4ea9e98433b4f9fdeec629c6983ed7e21e3f1
Alexey <geheim79@mail.ru>
2026-07-26 19:13:50 +0500

    Добавил постер и жанр фильма в компоненту MovieDetails

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,9 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getMovieDetails } from "../../services/movieService";
 
 function MovieDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const [movie, setMovie] = useState(null);
 
@@ -21,18 +22,45 @@ function MovieDetails() {
   }
 
   return (
-    <div>
-      <h1>{movie.title}</h1>
+  <div>
 
-      <p>
-        ⭐ Рейтинг: {movie.vote_average}
-      </p>
+    <button onClick={() => navigate(-1)}>
+      Назад
+    </button>
 
-      <p>
-        {movie.overview}
-      </p>
-    </div>
-  );
+    {movie.poster_path && (
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+        width="300"
+      />
+    )}
+
+    <h1>{movie.title}</h1>
+
+    <p>
+      ⭐ Рейтинг: {movie.vote_average}
+    </p>
+
+    <p>
+      📅 Дата выхода: {movie.release_date}
+    </p>
+
+    <p>
+      Жанр:
+      {movie.genres.map((genre) => (
+        <span key={genre.id}>
+          {" "}{genre.name}
+        </span>
+      ))}
+    </p>
+
+    <p>
+      {movie.overview}
+    </p>
+
+  </div>
+);
 }
 
 export default MovieDetails;

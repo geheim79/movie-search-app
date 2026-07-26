a106652bcf78fa46ccfd817fc6f2461a1bbbd1b2
Alexey <geheim79@mail.ru>
2026-07-26 14:38:46 +0500

    Подтверждение ввода по Enter

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,21 +13,27 @@ function Home() {
   // новое состояние ошибки
   const [error, setError] = useState("");
 
-  // Универсальная функция загрузки фильмов
-async function loadMovies(searchQuery) {
-  setLoading(true);
-  setError("");
+  // Асинхронная функция загрузки фильмов
+  async function loadMovies(searchQuery) {
+    setLoading(true);
+    setError("");
 
-  try {
-    const fetchedMovies = await searchMovies(searchQuery);
-    setMovies(fetchedMovies);
-  } catch (error) {
-    console.error(error);
-    setError("Не удалось загрузить фильмы.");
-  } finally {
-    setLoading(false);
+    try {
+      const fetchedMovies = await searchMovies(searchQuery);
+      setMovies(fetchedMovies);
+    } catch (error) {
+      console.error(error);
+      setError("Не удалось загрузить фильмы.");
+    } finally {
+      setLoading(false);
+    }
   }
-}
+  // обработчик подтверждения  по энтер
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    loadMovies(query);
+  }
 
   // При первом открытии страницы загружаем Batman
   useEffect(() => {
@@ -38,14 +44,16 @@ async function loadMovies(searchQuery) {
     <div>
       <h1>Главная страница</h1>
 
-      <input
-        type="text"
-        placeholder="Введите название фильма..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Введите название фильма..."
+        />
 
-      <button onClick={() => loadMovies(query)}>Найти</button>
+        <button type="submit">Найти</button>
+      </form>
       {loading && <h2>Загрузка...</h2>}
       {error && <h2>{error}</h2>}
       {movies.map((movie) => (
